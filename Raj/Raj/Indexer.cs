b14ee80f78dd05a5ff5a327edb10325f2bcf8eb2
godgiftedboy <61019893+godgiftedboy@users.noreply.github.com>
2023-04-03 07:59:30 +0545

    Added:
    Indexer, Array list, Hash table, Generic List

---
+++ Raj/Raj/Indexer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raj
+{
+    partial class Indexer
+    {
+        public string[] names = new string[5];
+        public string this[int i] 
+        {
+            get { return names[i]; }
+            set { names[i] = value; }
+        }
+    }
+    partial class Indexer
+    {
+        public string college;
+        /*
+        public string[] names = new string[5];
+        public string this[int i] 
+        {
+            get { return names[i]; }
+            set { names[i] = value; }
+        }
+        */
+    }
+}
--- Raj/Raj/Program.cs
+++ Raj/Raj/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Linq;
@@ -10,25 +11,78 @@ namespace Raj
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            Animal cow = new Animal();
-            cow.WhatIEat();
-            cow.WhatIEat("MOMO");
-            cow.WhatIEat("Samosa", "Chowmein");
-            Console.ReadKey();  
+            List<int> Roll = new List<int>();
+            Roll.Add(1);
+            Roll.Add(2);
+            Roll.Add(3);
+            Roll.Add(4);
+            Roll.Add(5);
+            foreach (int i in Roll) 
+            {
+                Console.WriteLine("Roll= "+i);
+            }
+            Console.ReadKey();
+        }   
+            /*
+            //HASH TABLE
+            Hashtable ht = new Hashtable();
+            ht.Add("College", "BMC");
+            ht.Add("Mobile", 9841);
+            ht.Add("IsMarried?", false);
+            foreach(var item in ht.Keys)
+            {
+                Console.WriteLine("Key is: {0} and value is {1}",item,ht[item]);
+            }
+            */
+           /*
+            //ARRAY LIST
+            ArrayList mylist = new ArrayList();
+            mylist.Add("RAK");
+            mylist.Add("Shyan");
+            mylist.Add(1);
+            mylist.Add(true);
+            mylist.Add(123);
+            foreach (var item in mylist) 
+            {
+                Console.WriteLine(item);
+            }
+           */
+            /*
+            //INDEXER
+            Indexer indexer=new Indexer();
+            indexer[0] = "Raj";
+            indexer[1] = "God";
+            indexer[2] = "GiFTeD";
+            indexer.college = "BMC";
+            for(int i = 0; i <= 2; i++) 
+            {
+                Console.WriteLine(indexer[i]);
+                Console.WriteLine("College = " + indexer.college);
+            }
+            Console.ReadKey();
+            */
+        /*
+        //FUNCTION OVERLOADING
+        Animal cow = new Animal();
+        cow.WhatIEat();
+        cow.WhatIEat("MOMO");
+        cow.WhatIEat("Samosa", "Chowmein");
+        Console.ReadKey();
+        */
 
-            
-            //Person Raj = new Person();
-            //Raj.HairColor = "black";
-            //Raj.NumberOfEyes = 2;
-            //Console.WriteLine("Raj has " + Raj.HairColor + " colored hair");
-            //Console.WriteLine("Raj has " + Raj.NumberOfEyes + " Eyes");
+        //Person Raj = new Person();
+        //Raj.HairColor = "black";
+        //Raj.NumberOfEyes = 2;
+        //Console.WriteLine("Raj has " + Raj.HairColor + " colored hair");
+        //Console.WriteLine("Raj has " + Raj.NumberOfEyes + " Eyes");
 
-            //BitwiseOperator();
-            //LogicalOperation();
-            //Console.ReadKey();
-        }
+        //BitwiseOperator();
+        //LogicalOperation();
+        //Console.ReadKey();
+
         static void LogicalOperation()
         {
             Console.WriteLine("Enter College Name:");
